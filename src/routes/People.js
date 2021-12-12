d2c28c864c7598004a64931e5a7d76bf47a12978
phil <pmass312@mtroyal.ca>
2021-12-11 19:21:26 -0500

    routing

---
+++ src/routes/People.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import get from api client
+
+class People extends React.Component {
+    render () {
+        return(
+            <div className="data">
+                <p>People rendered</p>
+            </div>
+        );
+    }
+}
+export default People;
